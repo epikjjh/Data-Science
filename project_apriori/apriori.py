4ae91ff1998340357077e6847f7ca0fccaa297c1
2015004248 <epikjjh@gmail.com>
2020-04-03 18:59:35 +0900

    04.03 Commit Make association rule need to refactoring
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- project_apriori/apriori.py
+++ project_apriori/apriori.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import combinations
 
 def verify(cand):
     # Convert set to list for indexing
@@ -19,7 +20,7 @@ def verify(cand):
     return ret_set        
 
 
-def generate(vcand,length):
+def generate_freq_list(vcand,length):
     ncand = [vcand[i].union(vcand[j]) for i in range(len(vcand)) for j in range(i+1,len(vcand))]
     ncand = list(set(tuple(sorted(elem)) for elem in ncand))
     ncand = list(filter(lambda x: len(x)==length, ncand)) 
@@ -27,25 +28,52 @@ def generate(vcand,length):
     return ncand
 
 
+def generate_association_rule(freq_list):
+    ret = []
+    for elem in freq_list:
+        set_length = len(elem[0])
+        if set_length <= 1:
+            continue
+        for e in elem:
+            for l in range(1,set_length):
+                total_combinations = combinations(e,l)       
+                for combination in total_combinations:
+                    left = set(combination)
+                    right = e - left
+                    total_cnt=0; left_cnt=0
+                    for data in table.values():
+                        if data >= e:
+                            total_cnt += 1
+                        if data >= left:
+                            left_cnt += 1
+                    sup = float(total_cnt)/len(table)*100
+                    conf = float(total_cnt)/left_cnt*100
+                    ret.append("{}\t{}\t{:.2f}\t{:.2f}\n".format(str(left),str(right),sup,conf))
+    return ret
+
+
 if __name__ == "__main__":
     # Command line argument
     min_sup, input_file, output_file = sys.argv[1:]
-    min_sup = float(min_sup)
-    f = open(input_file, 'r')
+    min_sup = float(min_sup)/100
     # Create transaction table
-    table = {tid: set(map(int,line.split())) for tid,line in enumerate(f.readlines())}
-    f.close()
+    with open(input_file, 'r') as f:
+        table = {tid: set(map(int,line.split())) for tid,line in enumerate(f.readlines())}
+    # Convert ratio to count
+    min_sup = len(table)*min_sup
     # Create candidates: length 1
     items = {value for row in table.values() for value in row} 
     cand = [[{item} for item in items]]
-    v_list = [verify(cand[0])]
+    freq_list = [verify(cand[0])]
     max_length = 1
     while True:
-        cand.append(generate(v_list[-1],max_length+1))
+        cand.append(generate_freq_list(freq_list[-1],max_length+1))
         vcand = verify(cand[-1])
         if not vcand:
             break
-        v_list.append(vcand)
+        freq_list.append(vcand)
         max_length += 1
-    print(max_length)
-    print(v_list)
+    association_list = generate_association_rule(freq_list)
+    with open(output_file, 'w') as f:
+        for line in association_list:
+            f.write(line)

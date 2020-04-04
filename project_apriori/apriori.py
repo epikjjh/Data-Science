aa816e8d3f13c23e19b6b818e95bb962bd67127b
2015004248 <epikjjh@gmail.com>
2020-04-04 10:05:25 +0900

    04.04 Commit Do some refactoring Write comments
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- project_apriori/apriori.py
+++ project_apriori/apriori.py
@@ -1,79 +1,134 @@
 import sys
 from itertools import combinations
 
-def verify(cand):
-    # Convert set to list for indexing
-    #cand = list(map(list,cand))
-    c_table = {idx: 0 for idx, _ in enumerate(cand)}
-    for row in table.values():
-        for idx,elem in enumerate(cand):
-            flag = True
-            for e in elem:
-                if e not in row:
-                    flag = False
-                    break
-            if(flag):
-                c_table[idx] += 1
-    ret_idx = [idx for idx, val in c_table.items() if val>=min_sup]
-    ret_set = [cand[idx] for idx in ret_idx]
+'''
+    Feature: Prune candidates by support and length
+    Arguments: cand(type: list[set])
+    Return value type: dictionary(key: tuple, value: float]
+'''
+def prune(cand):
+    # Make counting table
+    cnt_table = {idx: 0 for idx, _ in enumerate(cand)}
+    for transaction in table:
+        for idx,itemset in enumerate(cand):
+            # Check whether each transaction is superset of each itemset
+            if transaction >= itemset:
+                cnt_table[idx] += 1
+    # Filter by minimum support
+    ret = {tuple(cand[key]): val/len(table)*100 for key, val in cnt_table.items() if val>=min_sup}
 
-    return ret_set        
-
-
-def generate_freq_list(vcand,length):
-    ncand = [vcand[i].union(vcand[j]) for i in range(len(vcand)) for j in range(i+1,len(vcand))]
-    ncand = list(set(tuple(sorted(elem)) for elem in ncand))
-    ncand = list(filter(lambda x: len(x)==length, ncand)) 
-    ncand = list(map(set,ncand))
-    return ncand
+    return ret        
 
 
+'''
+    Feature: Generate candidates by self-joining
+    Arguments: itemdict(type: dictionary(key: tuple, value: float)), length(type: integer)
+    Return value type: list[set]
+'''
+def self_join(itemdict,length):
+    # Convert type: tuple->set
+    itemsets = list(map(set,itemdict.keys()))
+    # Generate candidates by set union operator
+    ret = [itemsets[i]|itemsets[j] for i in range(len(itemsets)) for j in range(i+1,len(itemsets))]
+    # Filter by length
+    ret = list(filter(lambda x: len(x)==length, ret)) 
+    # Remove duplicates
+    ret = list(set(tuple(sorted(itemset)) for itemset in ret))
+    # Convert type: tuple->set 
+    ret = list(map(set,ret))
+    return ret
+
+
+'''
+    Feature: Gerneate association rules
+    Arguments: freq_list(type: list[dictionary(key: tuple. value: float)])
+    Return value type: list[string] 
+'''
 def generate_association_rule(freq_list):
     ret = []
-    for elem in freq_list:
-        set_length = len(elem[0])
-        if set_length <= 1:
-            continue
-        for e in elem:
-            for l in range(1,set_length):
-                total_combinations = combinations(e,l)       
+    for itemdict in freq_list:
+        for itemset,val in itemdict.items():
+            # Exception: length is less or equal than 1
+            if len(itemset) <= 1:
+                break
+            # Save support value
+            sup = val
+            # Convert type: tuple->set 
+            itemset = set(itemset)
+            for length in range(1,len(itemset)):
+                # Generate all combinations
+                total_combinations = combinations(itemset,length)       
                 for combination in total_combinations:
+                    # Convert type: tuple->set 
                     left = set(combination)
-                    right = e - left
-                    total_cnt=0; left_cnt=0
-                    for data in table.values():
-                        if data >= e:
-                            total_cnt += 1
-                        if data >= left:
+                    right = itemset - left
+                    left_cnt=0
+                    for transaction in table:
+                        if transaction >= left:
                             left_cnt += 1
-                    sup = float(total_cnt)/len(table)*100
-                    conf = float(total_cnt)/left_cnt*100
+                    # Calculate confidence
+                    conf = sup*len(table)/left_cnt
+                    # Save result as string
                     ret.append("{}\t{}\t{:.2f}\t{:.2f}\n".format(str(left),str(right),sup,conf))
     return ret
 
 
 if __name__ == "__main__":
-    # Command line argument
+    '''
+        Initialize arguments
+        [Type]
+            min_sup: float(ratio)->integer(count)
+            input_file: string
+            output_file: string
+    '''
+    # Save command line argument
     min_sup, input_file, output_file = sys.argv[1:]
+    # Convert type: string->float
     min_sup = float(min_sup)/100
-    # Create transaction table
+
+    ''' 
+        Read input file
+        & Create transaction table
+        [Type]
+            table: list[sorted sets]
+    '''
     with open(input_file, 'r') as f:
-        table = {tid: set(map(int,line.split())) for tid,line in enumerate(f.readlines())}
-    # Convert ratio to count
+        table = [set(sorted(map(int,line.split()))) for line in f.readlines()]
+
+    # Convert: ratio->count
     min_sup = len(table)*min_sup
+
+    '''
+        Generate candidates(Self-joining)
+        & Prune candidates
+        => Generate frequent item sets depending on its length
+        [Type]
+            itmes: set
+            cand_list: list[list[set]] 
+            freq_list: list[dictionary(key:tuple, value=float(support))]
+            max_length: integer
+    '''
     # Create candidates: length 1
-    items = {value for row in table.values() for value in row} 
-    cand = [[{item} for item in items]]
-    freq_list = [verify(cand[0])]
+    items = {item_id for transaction in table for item_id in transaction} 
+    cand_list = [[{item_id} for item_id in items]]
+    freq_list = [prune(cand_list[0])]
     max_length = 1
+    # Create candidates depending on its length
     while True:
-        cand.append(generate_freq_list(freq_list[-1],max_length+1))
-        vcand = verify(cand[-1])
-        if not vcand:
+        cand_list.append(self_join(freq_list[-1],max_length+1))
+        tmp = prune(cand_list[-1])
+        if not tmp:
             break
-        freq_list.append(vcand)
+        freq_list.append(tmp)
         max_length += 1
+
+    '''
+        Generate association rules
+        [Type]
+            association_list: list[string]
+    '''
     association_list = generate_association_rule(freq_list)
+    # Write results in output file 
     with open(output_file, 'w') as f:
         for line in association_list:
             f.write(line)

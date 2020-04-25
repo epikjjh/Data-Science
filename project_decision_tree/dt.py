6504c935d03941919d5cb916e19d354c849d5690
2015004248 <epikjjh@gmail.com>
2020-04-25 18:29:15 +0900

    04.25 Commit Project: decision tree implement gini index
    Signed-off-by: 2015004248 <epikjjh@gmail.com>

--- project_decision_tree/dt.py
+++ project_decision_tree/dt.py
@@ -67,13 +67,46 @@ class DecisionTree:
         return gain/split_info
 
 
-    #def _calculate_gini_index(self):
+    # Calculate Gini(D)
+    def _calculate_gini(self, data_set):
+        ret = 1.0
+        total = len(data_set)
+        for _,val in data_set[self.label].value_counts().iteritems():
+            partial = val/total
+            ret -= partial**2
+
+        return ret
+
+
+    # Calculate GiniA(D)
+    def _calculate_gini_index(self, data_set, attribute, left, right):
+        total = len(data_set)
+        # Left
+        df_left = data_set[data_set[attribute].isin(left)]
+        left_scale = len(df_left)/total
+        left_gini = self._calculate_gini(df_left)
+        # Right
+        df_right = data_set[data_set[attribute].isin(right)]
+        right_scale = len(df_right)/total
+        right_gini = self._calculate_gini(df_right)
+
+        return left_scale*left_gini + right_scale*right_gini 
+
+
+    def _choose_branch(self, data_set, attribute):
+        atypes = self.attribute_dict[attribute]
+        gini_dict = {}
+        for i in range(1,len(atypes)):
+            left = tuple(atypes[:i])
+            right = tuple(atypes[i:])
+            gini_dict[(left,right)] = self._calculate_gini_index(data_set, attribute, left, right)
+        # Sort dictionary by value: ascending -> tuple((left,right), gini index)
+        return sorted(gini_dict.items(),key=lambda x: x[1], reverse=False)[0][0]
 
 
     def _majority_vote(self, data_set):
         major = df_train[self.label].value_counts().sort_values(ascending=False)
         return major.index[0]
-            
 
 
     # Select best attribute
@@ -81,13 +114,13 @@ class DecisionTree:
         # Type 0: Use information gain
         if self.metric_type == 0:
             info_dict = {attribute: self._calculate_information_gain(data_set,attribute) for attribute in data_set.columns if attribute!=self.label}
-            # Sort dictionary by key: descending -> tuple(attribute, information gain)
+            # Sort dictionary by value: descending -> tuple(attribute, information gain)
             return sorted(info_dict.items(),key=lambda x: x[1], reverse=True)[0][0]
 
         # Type 1: Use gain ratio 
         elif self.metric_type == 1:
             ratio_dict = {attribute: self._calculate_gain_ratio(data_set,attribute) for attribute in data_set.columns if attribute!=self.label}
-            # Sort dictionary by key: descending -> tuple(attribute, gain ratio) 
+            # Sort dictionary by value: descending -> tuple(attribute, gain ratio) 
             return  sorted(ratio_dict.items(),key=lambda x: x[1], reverse=True)[0][0]
 
 
@@ -96,7 +129,7 @@ class DecisionTree:
         target_att = self._select_attribute(data_set)
         return TreeNode(target_att)
 
-
+    
     # Build tree
     def _build_tree(self, data_set):
         # Data is classified perfectly
@@ -110,9 +143,19 @@ class DecisionTree:
             major = self._majority_vote(data_set)
             return TreeNode(None,True,major)
 
-        # Build node
+        # Build node & choose attribute
         node = self._build_node(data_set)
-
+        # Choose proper branch by using gini index -> binary tree
+        for branch in self._choose_branch(data_set, node.attribute):
+            #df_filtered = data_set[data_set[node.attribute].isin(branch)].drop(node.attribute, axis=1)
+            df_filtered = data_set[data_set[node.attribute].isin(branch)]
+            if len(df_filtered) > 0:
+                node.child[tuple(branch)] = self._build_tree(df_filtered)
+            # There are no remaining tuples -> majority voting
+            else:
+                major = self._majority_vote(data_set)
+                node.child[tuple(branch)] = TreeNode(None,True,major) 
+        '''
         # Split data set
         for atype in self.attribute_dict[node.attribute]:
             df_filtered = data_set[data_set[node.attribute]==atype].drop(node.attribute, axis=1)
@@ -122,7 +165,7 @@ class DecisionTree:
             else:
                 major = self._majority_vote(data_set)
                 node.child[atype] = TreeNode(None,True,major) 
-
+        '''
         return node
 
 
@@ -130,8 +173,13 @@ class DecisionTree:
     def _traverse_tree(self, tuple_data):
         node = self.root
         atype = tuple_data[node.attribute]
+        depth = 0
         while not node.isleaf:
-            node = node.child[atype]
+            # Select branch
+            for branch, nxt_node in node.child.items():
+                if atype in branch:
+                    break
+            node = nxt_node
             if node.attribute:
                 atype = tuple_data[node.attribute]
 
